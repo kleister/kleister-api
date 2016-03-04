df9f26640d0e31fc5cfa6346c390bf66493f5c19
Thomas Boerger <tboerger@suse.de>
2016-03-04 14:52:21 +0100

    Added minecraft list and refresh actions

--- controller/minecraft.go
+++ controller/minecraft.go
@@ -1,21 +1,80 @@
 package controller
 
 import (
-	"net/http"
-
+	"github.com/Sirupsen/logrus"
 	"github.com/gin-gonic/gin"
+	"github.com/solderapp/solder/model"
+	"github.com/solderapp/solder/model/minecraft"
+	"github.com/solderapp/solder/router/middleware/context"
 )
 
 func GetMinecraft(c *gin.Context) {
+	minecrafts := &model.Minecrafts{}
+	context.Store(c).Order("name DESC").Find(&minecrafts)
+
 	c.IndentedJSON(
-		http.StatusOK,
-		gin.H{},
+		200,
+		minecrafts,
 	)
 }
 
 func PatchMinecraft(c *gin.Context) {
+	result, err := minecraft.Load()
+
+	if err != nil {
+		logrus.Warn(err)
+
+		c.IndentedJSON(
+			422,
+			gin.H{
+				"status":  422,
+				"message": "Failed to request Minecraft versions",
+			},
+		)
+
+		return
+	}
+
+	for _, version := range result.Versions {
+		record := &model.Minecraft{}
+
+		if version.Invalid() {
+			continue
+		}
+
+		logrus.Debug(version)
+
+		err := context.Store(c).Where(
+			model.Minecraft{
+				Name: version.ID,
+			},
+		).Attrs(
+			model.Minecraft{
+				Type: version.Type,
+				URL:  version.URL,
+			},
+		).FirstOrCreate(
+			&record,
+		).Error
+
+		if err != nil {
+			c.IndentedJSON(
+				422,
+				gin.H{
+					"status":  422,
+					"message": "Failed to store Minecraft versions",
+				},
+			)
+
+			return
+		}
+	}
+
 	c.IndentedJSON(
-		http.StatusOK,
-		gin.H{},
+		200,
+		gin.H{
+			"status":  200,
+			"message": "Successfully imported Minecraft versions",
+		},
 	)
 }
--- model/minecraft.go
+++ model/minecraft.go
@@ -1,14 +1,28 @@
 package model
 
 import (
-	"github.com/jinzhu/gorm"
+	"time"
+
+	"github.com/satori/go.uuid"
 )
 
-type Minecraft struct {
-	gorm.Model
+type Minecrafts []*Minecraft
 
-	Slug   string   `json:"slug" sql:"unique_index"`
-	Name   string   `json:"version" sql:"unique_index"`
-	MD5    string   `json:"md5"`
-	Builds []*Build `json:"builds"`
+type Minecraft struct {
+	ID        int64     `json:"id" gorm:"primary_key"`
+	Slug      string    `json:"slug" sql:"unique_index"`
+	Name      string    `json:"version" sql:"unique_index"`
+	Type      string    `json:"type"`
+	URL       string    `json:"url"`
+	CreatedAt time.Time `json:"created_at"`
+	UpdatedAt time.Time `json:"updated_at"`
+	Builds    []*Build  `json:"-"`
+}
+
+func (u *Minecraft) BeforeSave() (err error) {
+	if u.Slug == "" {
+		u.Slug = uuid.NewV4().String()
+	}
+
+	return nil
 }
