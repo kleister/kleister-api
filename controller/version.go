add08352aaef969b03cbdb7fb43d34e937bd8d72
Thomas Boerger <tboerger@suse.de>
2016-03-08 17:49:05 +0100

    Integrated basic version actions

--- controller/version.go
+++ controller/version.go
@@ -4,44 +4,168 @@ import (
 	"net/http"
 
 	"github.com/gin-gonic/gin"
+	"github.com/solderapp/solder/model"
+	"github.com/solderapp/solder/router/middleware/context"
+	"github.com/solderapp/solder/router/middleware/session"
 )
 
 // GetVersions retrieves all available versions.
 func GetVersions(c *gin.Context) {
+	mod := session.Mod(c)
+	records := &model.Versions{}
+
+	err := context.Store(c).Scopes(
+		model.VersionDefaultOrder,
+	).Model(
+		&model.Version{},
+	).Where(
+		"versions.mod_id = ?",
+		mod.ID,
+	).Find(
+		&records,
+	).Error
+
+	if err != nil {
+		c.JSON(
+			http.StatusInternalServerError,
+			gin.H{
+				"status":  http.StatusInternalServerError,
+				"message": "Failed to fetch versions",
+			},
+		)
+
+		c.Abort()
+		return
+	}
+
 	c.JSON(
 		http.StatusOK,
-		gin.H{},
+		records,
 	)
 }
 
 // GetVersion retrieves a specific version.
 func GetVersion(c *gin.Context) {
+	record := session.Version(c)
+
 	c.JSON(
 		http.StatusOK,
-		gin.H{},
+		record,
 	)
 }
 
 // DeleteVersion removes a specific version.
 func DeleteVersion(c *gin.Context) {
+	record := session.Version(c)
+
+	err := context.Store(c).Delete(
+		&record,
+	).Error
+
+	if err != nil {
+		c.JSON(
+			http.StatusBadRequest,
+			gin.H{
+				"status":  http.StatusBadRequest,
+				"message": err.Error(),
+			},
+		)
+
+		c.Abort()
+		return
+	}
+
 	c.JSON(
 		http.StatusOK,
-		gin.H{},
+		gin.H{
+			"status":  http.StatusOK,
+			"message": "Successfully deleted version",
+		},
 	)
 }
 
 // PatchVersion updates an existing version.
 func PatchVersion(c *gin.Context) {
+	record := session.Version(c)
+
+	if err := c.BindJSON(&record); err != nil {
+		c.JSON(
+			http.StatusPreconditionFailed,
+			gin.H{
+				"status":  http.StatusPreconditionFailed,
+				"message": "Failed to bind version data",
+			},
+		)
+
+		c.Abort()
+		return
+	}
+
+	err := context.Store(c).Save(
+		&record,
+	).Error
+
+	if err != nil {
+		c.JSON(
+			http.StatusBadRequest,
+			gin.H{
+				"status":  http.StatusBadRequest,
+				"message": err.Error(),
+			},
+		)
+
+		c.Abort()
+		return
+	}
+
 	c.JSON(
 		http.StatusOK,
-		gin.H{},
+		record,
 	)
 }
 
 // PostVersion creates a new version.
 func PostVersion(c *gin.Context) {
+	mod := session.Mod(c)
+
+	record := &model.Version{
+		ModID: mod.ID,
+	}
+
+	record.Defaults()
+
+	if err := c.BindJSON(&record); err != nil {
+		c.JSON(
+			http.StatusPreconditionFailed,
+			gin.H{
+				"status":  http.StatusPreconditionFailed,
+				"message": "Failed to bind version data",
+			},
+		)
+
+		c.Abort()
+		return
+	}
+
+	err := context.Store(c).Create(
+		&record,
+	).Error
+
+	if err != nil {
+		c.JSON(
+			http.StatusBadRequest,
+			gin.H{
+				"status":  http.StatusBadRequest,
+				"message": err.Error(),
+			},
+		)
+
+		c.Abort()
+		return
+	}
+
 	c.JSON(
 		http.StatusOK,
-		gin.H{},
+		record,
 	)
 }
--- model/version.go
+++ model/version.go
@@ -1,10 +1,27 @@
 package model
 
 import (
+	"fmt"
 	"time"
 
-	_ "github.com/Machiel/slugify"
+	"github.com/Machiel/slugify"
+	"github.com/jinzhu/gorm"
 )
+
+const (
+	// VersionUsernameMinLength is the minimum length of the username.
+	VersionUsernameMinLength = "3"
+
+	// VersionUsernameMaxLength is the maximum length of the username.
+	VersionUsernameMaxLength = "255"
+)
+
+// VersionDefaultOrder is the default ordering for pack listings.
+func VersionDefaultOrder(db *gorm.DB) *gorm.DB {
+	return db.Order(
+		"versions.name ASC",
+	)
+}
 
 // Versions is simply a collection of version structs.
 type Versions []*Version
@@ -13,10 +30,34 @@ type Versions []*Version
 type Version struct {
 	ID        int64     `json:"id" gorm:"primary_key"`
 	Mod       *Mod      `json:"mod"`
-	ModID     int       `json:"mod_id" sql:"index"`
+	ModID     int64     `json:"mod_id" sql:"index"`
 	Slug      string    `json:"slug" sql:"unique_index"`
 	Name      string    `json:"name" sql:"unique_index"`
 	MD5       string    `json:"md5"`
 	CreatedAt time.Time `json:"created_at"`
 	UpdatedAt time.Time `json:"updated_at"`
+}
+
+// BeforeSave invokes required actions before persisting.
+func (u *Version) BeforeSave(db *gorm.DB) (err error) {
+	if u.Slug == "" {
+
+		u.Slug = slugify.Slugify(u.Name)
+		// Fill the slug with slugified name
+
+	}
+
+	return nil
+}
+
+// Defaults prefills the struct with some default values.
+func (u *Version) Defaults() {
+	// Currently no default values required.
+}
+
+// Validate does some validation to be able to store the record.
+func (u *Version) Validate(db *gorm.DB) {
+	if u.Name == "" {
+		db.AddError(fmt.Errorf("Name is a required attribute"))
+	}
 }
