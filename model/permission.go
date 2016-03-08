dba698705ff2246ebe35d0d5b3ccc0175111f5ce
Thomas Boerger <tboerger@suse.de>
2016-03-08 15:26:56 +0100

    Changed permission structure

--- model/permission.go
+++ model/permission.go
@@ -4,28 +4,35 @@ import (
 	"time"
 )
 
+// Permissions is simply a collection of permission structs.
+type Permissions []*Permission
+
 // Permission represents a permission model definition.
 type Permission struct {
-	ID            int64     `json:"id" gorm:"primary_key"`
-	User          *User     `json:"-"`
-	UserID        int       `json:"user_id" sql:"index"`
-	Admin         bool      `json:"admin" sql:"default:false"`
-	CreateUsers   bool      `json:"create_users" sql:"default:false"`
-	DeleteUsers   bool      `json:"delete_users" sql:"default:false"`
-	ManageUsers   bool      `json:"manage_users" sql:"default:false"`
-	CreateKeys    bool      `json:"create_keys" sql:"default:false"`
-	DeleteKeys    bool      `json:"delete_keys" sql:"default:false"`
-	ManageKeys    bool      `json:"manage_keys" sql:"default:false"`
-	CreateClients bool      `json:"create_clients" sql:"default:false"`
-	DeleteClients bool      `json:"delete_clients" sql:"default:false"`
-	ManageClients bool      `json:"manage_clients" sql:"default:false"`
-	CreatePacks   bool      `json:"create_packs" sql:"default:false"`
-	DeletePacks   bool      `json:"delete_packs" sql:"default:false"`
-	ManagePacks   bool      `json:"manage_packs" sql:"default:false"`
-	CreateMods    bool      `json:"create_mods" sql:"default:false"`
-	DeleteMods    bool      `json:"delete_mods" sql:"default:false"`
-	ManageMods    bool      `json:"manage_mods" sql:"default:false"`
-	CreatedAt     time.Time `json:"created_at"`
-	UpdatedAt     time.Time `json:"updated_at"`
-	Packs         *Packs    `json:"packs" gorm:"many2many:permission_packs;"`
+	ID             int64     `json:"-" gorm:"primary_key"`
+	User           *User     `json:"-"`
+	UserID         int       `json:"-" sql:"index"`
+	DisplayUsers   bool      `json:"display_users" sql:"default:false"`
+	ChangeUsers    bool      `json:"change_users" sql:"default:false"`
+	DeleteUsers    bool      `json:"delete_users" sql:"default:false"`
+	DisplayKeys    bool      `json:"display_keys" sql:"default:false"`
+	ChangeKeys     bool      `json:"change_keys" sql:"default:false"`
+	DeleteKeys     bool      `json:"delete_keys" sql:"default:false"`
+	DisplayClients bool      `json:"display_clients" sql:"default:false"`
+	ChangeClients  bool      `json:"change_clients" sql:"default:false"`
+	DeleteClients  bool      `json:"delete_clients" sql:"default:false"`
+	DisplayPacks   bool      `json:"display_packs" sql:"default:false"`
+	ChangePacks    bool      `json:"change_packs" sql:"default:false"`
+	DeletePacks    bool      `json:"delete_packs" sql:"default:false"`
+	DisplayMods    bool      `json:"display_mods" sql:"default:false"`
+	ChangeMods     bool      `json:"change_mods" sql:"default:false"`
+	DeleteMods     bool      `json:"delete_mods" sql:"default:false"`
+	DisplaydAt     time.Time `json:"-"`
+	UpdatedAt      time.Time `json:"-"`
+	Packs          *Packs    `json:"packs" gorm:"many2many:permission_packs;"`
+}
+
+// Defaults prefills the struct with some default values.
+func (u *Permission) Defaults() {
+	// Currently no default values required.
 }
