8d46088895fc2002dcf1d3b6ce465978e4a93b76
Thomas Boerger <thomas@webhippie.de>
2016-03-08 21:19:09 +0100

    Moved database config to the top level

--- cmd/server.go
+++ cmd/server.go
@@ -16,41 +16,6 @@ func Server(cfg *config.Config) cli.Command {
 		Usage: "Start the Solder server",
 		Flags: []cli.Flag{
 			cli.StringFlag{
-				Name:        "db-driver",
-				Value:       "mysql",
-				Usage:       "Database driver selection",
-				EnvVar:      "SOLDER_DB_DRIVER",
-				Destination: &cfg.Database.Driver,
-			},
-			cli.StringFlag{
-				Name:        "db-username",
-				Value:       "root",
-				Usage:       "Username for database connection",
-				EnvVar:      "SOLDER_DB_USERNAME",
-				Destination: &cfg.Database.Username,
-			},
-			cli.StringFlag{
-				Name:        "db-password",
-				Value:       "root",
-				Usage:       "Password for database connection",
-				EnvVar:      "SOLDER_DB_PASSWORD",
-				Destination: &cfg.Database.Password,
-			},
-			cli.StringFlag{
-				Name:        "db-name",
-				Value:       "solder",
-				Usage:       "Name for database connection",
-				EnvVar:      "SOLDER_DB_NAME",
-				Destination: &cfg.Database.Name,
-			},
-			cli.StringFlag{
-				Name:        "db-host",
-				Value:       "localhost:3306",
-				Usage:       "Host for database connection",
-				EnvVar:      "SOLDER_DB_HOST",
-				Destination: &cfg.Database.Host,
-			},
-			cli.StringFlag{
 				Name:        "addr",
 				Value:       ":8080",
 				Usage:       "Address to bind the server",
