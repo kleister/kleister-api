5ad1f3022cf54c2af177842bd6f1fbb5d0cf1328
Thomas Boerger <tboerger@suse.de>
2016-03-08 16:30:09 +0100

    Integrated profile show and update

--- controller/profile.go
+++ controller/profile.go
@@ -4,20 +4,56 @@ import (
 	"net/http"
 
 	"github.com/gin-gonic/gin"
+	"github.com/solderapp/solder/router/middleware/context"
+	"github.com/solderapp/solder/router/middleware/session"
 )
 
 // GetProfile displays the current profile.
 func GetProfile(c *gin.Context) {
+	record := session.Current(c)
+
 	c.JSON(
 		http.StatusOK,
-		gin.H{},
+		record,
 	)
 }
 
 // PatchProfile updates the current profile.
 func PatchProfile(c *gin.Context) {
+	record := session.Current(c)
+
+	if err := c.BindJSON(&record); err != nil {
+		c.JSON(
+			http.StatusPreconditionFailed,
+			gin.H{
+				"status":  http.StatusPreconditionFailed,
+				"message": "Failed to bind profile data",
+			},
+		)
+
+		c.Abort()
+		return
+	}
+
+	err := context.Store(c).Save(
+		&record,
+	).Error
+
+	if err != nil {
+		c.JSON(
+			http.StatusBadRequest,
+			gin.H{
+				"status":  http.StatusBadRequest,
+				"message": err.Error(),
+			},
+		)
+
+		c.Abort()
+		return
+	}
+
 	c.JSON(
 		http.StatusOK,
-		gin.H{},
+		record,
 	)
 }
