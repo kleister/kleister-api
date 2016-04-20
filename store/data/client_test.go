b2513ee11a47a481055de7041c4efd101dc86541
Thomas Boerger <tboerger@suse.de>
2016-04-20 14:47:11 +0200

    Added more tests

--- store/data/client_test.go
+++ store/data/client_test.go
@@ -1,41 +1,105 @@
 package data
 
 import (
-  "testing"
-  "strconv"
+	"strconv"
+	"testing"
 
-  "github.com/solderapp/solder-api/model"
-  . "github.com/franela/goblin"
+	. "github.com/franela/goblin"
+	. "github.com/onsi/gomega"
+	"github.com/solderapp/solder-api/model"
 )
 
 func TestClients(t *testing.T) {
-  store := Test()
+	g := Goblin(t)
+	RegisterFailHandler(func(m string, _ ...int) { g.Fail(m) })
 
-  g := Goblin(t)
-  g.Describe("Clients", func() {
-    g.BeforeEach(func() {
-      store.DeleteClient(&model.Client{})
-    })
+	store := Test()
 
-    g.It("Should get a Client", func() {
-      record := model.Client{
-        Name: "Client 1",
-        Value: "UUID1",
-      }
+	g.Describe("Clients", func() {
+		g.BeforeEach(func() {
+			store.DeleteClient(&model.Client{})
+		})
 
-      store.CreateClient(
-        &record,
-      )
+		g.Describe("GetClient", func() {
+			g.It("Should find a client", func() {
+				record := model.Client{
+					Name:  "Client 1",
+					Value: "UUID1",
+				}
 
-      result, res := store.GetClient(
-        strconv.Itoa(record.ID),
-      )
+				store.CreateClient(
+					&record,
+				)
 
-      // TODO(should): Check for existance of Slug
-      g.Assert(res.Error == nil).IsTrue()
-      g.Assert(record.ID).Equal(result.ID)
-      g.Assert(record.Name).Equal(result.Name)
-      g.Assert(record.Value).Equal(result.Value)
-    })
-  })
+				result, res := store.GetClient(
+					strconv.Itoa(record.ID),
+				)
+
+				Expect(res.RecordNotFound()).To(BeFalse())
+        Expect(res.Error).NotTo(HaveOccurred())
+				Expect(record.Slug).To(Equal("client-1"))
+				Expect(record.ID).To(Equal(result.ID))
+				Expect(record.Name).To(Equal(result.Name))
+				Expect(record.Value).To(Equal(result.Value))
+			})
+
+			g.It("Should return RecordNotFound", func() {
+				result, res := store.GetClient(
+					"foo",
+				)
+
+				Expect(res.RecordNotFound()).To(BeTrue())
+        Expect(res.Error).To(HaveOccurred())
+				Expect(result.ID).To(Equal(0))
+			})
+		})
+
+		g.Describe("GetClients", func() {
+			g.It("Should find clients", func() {
+
+			})
+		})
+
+		g.Describe("CreateClient", func() {
+
+		})
+
+		g.Describe("UpdateClient", func() {
+
+		})
+
+		g.Describe("DeleteClient", func() {
+			g.It("Should delete a client", func() {
+				record := model.Client{
+					Name:  "Client 1",
+					Value: "UUID1",
+				}
+
+				store.CreateClient(
+					&record,
+				)
+
+				err := store.DeleteClient(
+					&record,
+				)
+
+				_, res := store.GetClient(
+					strconv.Itoa(record.ID),
+				)
+
+				Expect(err).NotTo(HaveOccurred())
+				Expect(res.RecordNotFound()).To(BeTrue())
+			})
+
+			g.It("Should not result in an error", func() {
+				err := store.DeleteClient(
+					&model.Client{
+						ID: 1000,
+					},
+				)
+
+				Expect(err).NotTo(HaveOccurred())
+			})
+		})
+	})
 }
